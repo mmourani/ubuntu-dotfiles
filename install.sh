447add29fb3c7dac5d26ba2389ec819aa454bf8a
Maroun Mourani <mouranim@gmail.com>
2018-07-14 10:13:28 +0400

    modif

--- install.sh
+++ install.sh
@@ -30,54 +30,33 @@ fi
 
 grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
 if [[ $? = 0 ]]; then
+
     read -r -p "What is your github.com username? " githubuser
+    if [[ ! $githubuser ]];then
+      error "you must provide a username to configure .gitconfig"
+      exit 1
+    fi
 
-  fullname=`osascript -e "long user name of (system info)"`
-
-  if [[ -n "$fullname" ]];then
-    lastname=$(echo $fullname | awk '{print $2}');
-    firstname=$(echo $fullname | awk '{print $1}');
-  fi
-
-  if [[ -z $lastname ]]; then
-    lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
-  fi
-  if [[ -z $firstname ]]; then
-    firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
-  fi
-  email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
-
-  if [[ ! "$firstname" ]];then
-    response='n'
-  else
-    echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
-
-  if [[ $response =~ ^(no|n|N) ]];then
     read -r -p "What is your first name? " firstname
+    if [[ ! $firstname ]];then
+      error "you must provide a first name to configure .gitconfig"
+      exit 1
+    fi
+
     read -r -p "What is your last name? " lastname
-  fi
-  fullname="$firstname $lastname"
+    if [[ ! $lastname ]];then
+      error "you must provide a last name to configure .gitconfig"
+      exit 1
+    fi
+    fullname="$firstname $lastname"
+    bot "Great $fullname, "
 
-  bot "Great $fullname, "
-
-  if [[ ! $email ]];then
-    response='n'
-  else
-    echo -e "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
-
-  if [[ $response =~ ^(no|n|N) ]];then
     read -r -p "What is your email? " email
     if [[ ! $email ]];then
       error "you must provide an email to configure .gitconfig"
       exit 1
     fi
-  fi
-  
-  running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $email, $githubuser$COL_RESET)"
+    running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $email, $githubuser$COL_RESET)"
 
   # test if gnu-sed or MacOS sed
 
