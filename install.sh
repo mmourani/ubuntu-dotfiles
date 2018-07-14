9567accd0e2bfc3c22591954a4c74a9c53235570
Maroun Mourani <mouranim@gmail.com>
2018-07-14 09:26:13 +0400

    modif

--- install.sh
+++ install.sh
@@ -28,6 +28,55 @@ if [[ $response =~ (yes|y|Y) ]];then
     bot "Your /etc/hosts file has been updated. Last version is saved in /etc/hosts.backup"
 fi
 
+grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
+if [[ $? = 0 ]]; then
+    read -r -p "What is your github.com username? " githubuser
+
+  fullname=`osascript -e "long user name of (system info)"`
+
+  if [[ -n "$fullname" ]];then
+    lastname=$(echo $fullname | awk '{print $2}');
+    firstname=$(echo $fullname | awk '{print $1}');
+  fi
+
+  if [[ -z $lastname ]]; then
+    lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
+  fi
+  if [[ -z $firstname ]]; then
+    firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
+  fi
+  email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
+
+  if [[ ! "$firstname" ]];then
+    response='n'
+  else
+    echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
+    read -r -p "Is this correct? [Y|n] " response
+  fi
+
+  if [[ $response =~ ^(no|n|N) ]];then
+    read -r -p "What is your first name? " firstname
+    read -r -p "What is your last name? " lastname
+  fi
+  fullname="$firstname $lastname"
+
+  bot "Great $fullname, "
+
+  if [[ ! $email ]];then
+    response='n'
+  else
+    echo -e "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
+    read -r -p "Is this correct? [Y|n] " response
+  fi
+
+  if [[ $response =~ ^(no|n|N) ]];then
+    read -r -p "What is your email? " email
+    if [[ ! $email ]];then
+      error "you must provide an email to configure .gitconfig"
+      exit 1
+    fi
+  fi
+
 read -r -p " continue ? [Y|n] " response
 if [[ $response =~ (no|n|N) ]];then
     ok
