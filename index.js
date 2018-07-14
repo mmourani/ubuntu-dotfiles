5a82ce410f78fe88709fcae092c7c40826d595e8
Maroun Mourani <mouranim@gmail.com>
2018-07-14 16:03:30 +0400

    modif

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ inquirer.prompt([{
     console.info(emoji.get('coffee'), ' installing '+type+' packages')
     config[type].map(function(item){
       console.info(type+':', item)
-      command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_'+type+' ' + item, __dirname, function(err, out) {
+      command('npm i '+ item, __dirname, function(err, out) {
         if(err) console.error(emoji.get('fire'), err)
       })
     })
