fc4762b8e551befcfa7d4651802770288af3b9f6
Maroun Mourani <mouranim@gmail.com>
2018-07-14 11:39:11 +0400

    updated config file in homedir

--- install.sh
+++ install.sh
@@ -126,7 +126,24 @@ fi
   bot "creating symlinks for project dotfiles..."
   pushd homedir > /dev/null 2>&1
   now=$(date +"%Y.%m.%d.%H.%M.%S")
-
+  
+    for file in .*; do
+      if [[ $file == "." || $file == ".." ]]; then
+        continue
+      fi
+      running "~/$file"
+      # if the file exists:
+      if [[ -e ~/$file ]]; then
+          mkdir -p ~/.dotfiles_backup/$now
+          mv ~/$file ~/.dotfiles_backup/$now/$file
+          echo "backup saved as ~/.dotfiles_backup/$now/$file"
+      fi
+      # symlink might still exist
+      unlink ~/$file > /dev/null 2>&1
+      # create the link
+      ln -s ~/.dotfiles/homedir/$file ~/$file
+      echo -en '\tlinked';ok
+    done
 
 read -r -p " continue ? [Y|n] " response
 if [[ $response =~ (no|n|N) ]];then
